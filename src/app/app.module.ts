d99da56bf6676df0180a02ad9fcb68f1a2296233
Pierce Chang <143874526+Pierce-Chang@users.noreply.github.com>
2023-12-30 19:52:41 +0100

    header and first half of i am

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { WhoamiComponent } from './whoami/whoami.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    WhoamiComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/whoami/whoami.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-whoami',
+  templateUrl: './whoami.component.html',
+  styleUrl: './whoami.component.scss'
+})
+export class WhoamiComponent {
+
+}
