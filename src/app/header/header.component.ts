bd964b32a7448b00f934c9574283e10b8bdaa91c
Pierce Chang <143874526+Pierce-Chang@users.noreply.github.com>
2024-01-16 22:27:10 +0100

    started responsive design

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,20 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
-
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
   activeLink!: string;
+  isMenuOpen = false;
 
   setActiveLink(link: string): void {
-      this.activeLink = link;
+    this.activeLink = link;
+    this.isMenuOpen = false; // Schließt das Menü, nachdem ein Link ausgewählt wurde
+  }
+
+  toggleMenu(): void {
+    this.isMenuOpen = !this.isMenuOpen;
   }
 }
