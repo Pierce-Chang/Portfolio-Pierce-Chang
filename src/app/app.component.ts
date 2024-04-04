ef39d601b3b90321f9f1dc2c8701bb077969c50e
Pierce Chang <143874526+Pierce-Chang@users.noreply.github.com>
2024-04-04 21:09:37 +0200

    loading animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,30 @@
-import { AfterViewInit, Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent implements AfterViewInit{
+export class AppComponent implements OnInit {
   title = 'Pierce-Chang_Portfolio';
   isLoading: boolean = true;
+  message: string = '';
+  private fullMessage: string = 'Willkommen auf meinem Portfolio ...';
+  private currentIndex: number = 0;
 
-  ngAfterViewInit(): void {
-    setTimeout(() => {
-      this.isLoading = false;
-    }, 3000);
+  ngOnInit(): void {
+    this.typeMessage();
+  }
+
+  typeMessage(): void {
+    if (this.currentIndex < this.fullMessage.length) {
+      this.message += this.fullMessage.charAt(this.currentIndex);
+      this.currentIndex++;
+      setTimeout(() => this.typeMessage(), 50);
+    } else {
+      setTimeout(() => {
+        this.isLoading = false;
+      }, 1500);
+    }
   }
 }
