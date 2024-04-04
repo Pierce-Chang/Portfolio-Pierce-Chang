4a7f939cfac38e2e653d21e3199eca3d7f609095
Pierce Chang <143874526+Pierce-Chang@users.noreply.github.com>
2024-04-04 20:25:26 +0200

    started loading animation logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit{
   title = 'Pierce-Chang_Portfolio';
+  isLoading: boolean = true;
+
+  ngAfterViewInit(): void {
+    setTimeout(() => {
+      this.isLoading = false;
+    }, 3000);
+  }
 }
